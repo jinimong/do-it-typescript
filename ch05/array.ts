6f820e29e5d72e8776360d1ce3fed905a3d4c964
jwkim <phoongdeng@gmail.com>
2020-05-15 19:33:40 +0900

    array.ts : 배열은 Array 클래스의 인스턴스, [] 단축구문 지원

---
+++ ch05/array.ts
@@ -0,0 +1,9 @@
+let array = new Array
+array.push(1)
+array.push(2)
+array.push(3)
+console.log(array) // [ 1, 2, 3 ]
+
+let numbers = [1, 2, 3]
+let strings = ['Hello', 'World']
+console.log(numbers, strings) // [ 1, 2, 3 ] [ 'Hello', 'World' ]
