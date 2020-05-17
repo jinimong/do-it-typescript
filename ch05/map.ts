73f46a45f89393a6307a71fea3a51daf7adc69ad
jwkim <phoongdeng@gmail.com>
2020-05-17 22:51:08 +0900

    map 함수를 이용하여 제곱값의 합 구하기

---
+++ ch05/map.ts
@@ -0,0 +1,9 @@
+const map = <T, Q>(array: T[], callback: (value: T, index?: number) => Q): Q[] => {
+  let result: Q[] = []
+  for(let index = 0; index < array.length; ++index) {
+    const value = array[index]
+    result = [...result, callback(value, index)]
+  }
+  return result
+}
+export default map
