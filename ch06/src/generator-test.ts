e895ff328ba92120265b5e4c0790bff283800e8e
jwkim <phoongdeng@gmail.com>
2020-05-19 00:55:32 +0900

    제너레이터 예제

---
+++ ch06/src/generator-test.ts
@@ -0,0 +1,7 @@
+import generator from './generator'
+for (let value of generator()) console.log(value)
+// generator started...
+// 1
+// 2
+// 3
+// generator finishied...
---
+++ ch06/src/generator.ts
@@ -0,0 +1,6 @@
+export default function* generator() {
+  console.log('generator started...')
+  let value = 1
+  while (value < 4) yield value++
+  console.log('generator finishied...')
+}
