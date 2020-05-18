9e45db3f590be7760fd6643add9937ae839aa740
jwkim <phoongdeng@gmail.com>
2020-05-19 01:03:57 +0900

    setInterval 함수 세미코루틴 동작 예제

---
+++ ch06/src/setInterval.ts
@@ -0,0 +1,14 @@
+const period = 1000
+let count = 0
+console.log('program started...')
+const id = setInterval(() => {
+  if (count >= 3) {
+    clearInterval(id)
+    console.log('program finished...')
+  } else console.log(++count)
+}, period)
+// generator started...
+// 1
+// 2
+// 3
+// generator finishied...
