068f2dc1735ddf58537c4a84e0c9ea11594ace57
jwkim <phoongdeng@gmail.com>
2020-05-22 00:04:39 +0900

    StingIterator 예제

---
+++ ch06/src/StringIterable.ts
@@ -0,0 +1,23 @@
+class StringIterable implements Iterable<string> {
+  constructor(
+    private strings: string[] = [],
+    private currentIndex: number = 0
+  ) {}
+  [Symbol.iterator](): Iterator<string> {
+    const that = this
+    let currentIndex = that.currentIndex
+    const length = that.strings.length
+
+    const iterator: Iterator<string> = {
+      next(): { value: string; done: boolean } {
+        const value =
+          currentIndex < length ? that.strings[currentIndex++] : undefined
+        const done = value == undefined
+        return { value: value || '', done }
+      },
+    }
+    return iterator
+  }
+}
+
+export default StringIterable
