553d37e0346c183157f724bd938d2987bd7760db
jwkim <phoongdeng@gmail.com>
2020-05-22 20:54:09 +0900

    Prettier 세미콜론 추가로 즉시함수호출 방식으로 추가

--- ch07/src/test.ts
+++ ch07/src/test.ts
@@ -1,32 +1,30 @@
-import { readFileSync, readFile } from 'fs'
+import { readFileSync, readFile } from 'fs';
 
 // package.json 파일을 동기 방식으로 읽기
-console.log('read package.json using synchronous api...')
-const buffer: Buffer = readFileSync('../package.json')
-console.log(buffer.toString())
+console.log('read package.json using synchronous api...');
+const buffer: Buffer = readFileSync('../package.json');
+console.log(buffer.toString());
 
 // package.json 파일을 비동기 방식으로 읽기
 readFile(
   '../package.json',
   (error: NodeJS.ErrnoException | null, buffer: Buffer) => {
-    console.log('read package.json using asynchronous api...')
-    console.log(buffer.toString())
+    console.log('read package.json using asynchronous api...');
+    console.log(buffer.toString());
   }
-)
+);
 
 // Promise와 async/await 구문을 사용하여 읽기
 const readFilePromise = (filename: string): Promise<string> =>
   new Promise<string>(
     (resolve: (value: string) => void, reject: (error: Error) => void) =>
       readFile(filename, (error, buffer: Buffer) => {
-        if (error) reject(error)
-        else resolve(buffer.toString())
+        if (error) reject(error);
+        else resolve(buffer.toString());
       })
-  )
-
-const readFileAsync = async () => {
-  const content = await readFilePromise('../package.json')
-  console.log('read package.json using Promise and async/await...')
-  console.log(content)
-}
-readFileAsync()
+  );
+(async () => {
+  const content = await readFilePromise('../package.json');
+  console.log('read package.json using Promise and async/await...');
+  console.log(content);
+})();
