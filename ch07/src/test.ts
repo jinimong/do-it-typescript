5ac4d8b45bcb370c9275faaadbcaf0f14b2ca8c2
jwkim <phoongdeng@gmail.com>
2020-05-22 20:38:03 +0900

    test 예제에 파라미터 타입 추가

--- ch07/src/test.ts
+++ ch07/src/test.ts
@@ -6,18 +6,22 @@ const buffer: Buffer = readFileSync('../package.json')
 console.log(buffer.toString())
 
 // package.json 파일을 비동기 방식으로 읽기
-readFile('../package.json', (error, buffer: Buffer) => {
-  console.log('read package.json using asynchronous api...')
-  console.log(buffer.toString())
-})
+readFile(
+  '../package.json',
+  (error: NodeJS.ErrnoException | null, buffer: Buffer) => {
+    console.log('read package.json using asynchronous api...')
+    console.log(buffer.toString())
+  }
+)
 
 // Promise와 async/await 구문을 사용하여 읽기
 const readFilePromise = (filename: string): Promise<string> =>
-  new Promise<string>((resolve, reject) =>
-    readFile(filename, (error, buffer: Buffer) => {
-      if (error) reject(error)
-      else resolve(buffer.toString())
-    })
+  new Promise<string>(
+    (resolve: (value: string) => void, reject: (error: Error) => void) =>
+      readFile(filename, (error, buffer: Buffer) => {
+        if (error) reject(error)
+        else resolve(buffer.toString())
+      })
   )
 
 const readFileAsync = async () => {
