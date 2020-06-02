0551fdce63741a808a67f4a826fd84088ef52766
jwkim <phoongdeng@gmail.com>
2020-06-02 22:43:32 +0900

    chance 라이브러리를 이용해 Location 생성

---
+++ ch09/src/location-test.ts
@@ -0,0 +1,10 @@
+import { makeRandomILocation, ILocation } from './model/location';
+
+const location: ILocation = makeRandomILocation();
+console.log(location);
+/**
+{ country: 'CR',
+  city: 'Jizjuhov',
+  address: '810 Rewibe Loop',
+  coordinates: { latitude: 18.45822, longitude: -81.46254 } }
+ */
