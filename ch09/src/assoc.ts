b4b36e99442274c0497602c2b57bd6f4fc196cab
jwkim <phoongdeng@gmail.com>
2020-06-04 02:35:54 +0900

    R.prop, R.assoc 함수

---
+++ ch09/src/assoc.ts
@@ -0,0 +1,13 @@
+import * as R from 'ramda';
+import { IPerson, makeRandomIPerson } from './model/person';
+
+const getName = R.pipe(
+  R.prop('name'),
+  R.tap((name) => console.log(name))
+);
+
+const person: IPerson = makeRandomIPerson();
+const originalName = getName(person); // Elnora Lewis
+
+const modifiedPerson = R.assoc('name', 'Albert Einstein')(person);
+const modifiedName = getName(modifiedPerson); // Albert Einstein
