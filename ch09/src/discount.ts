22cb10d199d63b86c74d1dab83d666db4328c743
jwkim <phoongdeng@gmail.com>
2020-06-04 20:20:21 +0900

    R.ifElse 함수를 이용한 최소금액 할인조건 예제

---
+++ ch09/src/discount.ts
@@ -0,0 +1,12 @@
+import * as R from 'ramda';
+
+type NumToNumFunc = (n: number) => number;
+const applyDiscount = (minimum: number, discount: number): NumToNumFunc =>
+  R.pipe(
+    R.ifElse(R.flip(R.gte)(minimum), R.flip(R.subtract)(discount), R.identity),
+    R.tap((amount) => console.log(amount))
+  );
+const calcPrice = applyDiscount(5000, 500);
+
+const discountedPrice = calcPrice(6000); // 5500
+const notDiscountedPrice = calcPrice(4500); // 4500
