92cfdd257882dd63f8778e22c83daacbb2fed31d
jwkim <phoongdeng@gmail.com>
2020-06-07 23:43:21 +0900

    IApply<T> IApplicative<T> 인터페이스와 구현

--- ch11/src/classes/Identity.ts
+++ ch11/src/classes/Identity.ts
@@ -1,6 +1,6 @@
-import { IFunctor, ISetiod } from '../interfaces';
+import { IApplicative, ISetiod } from '../interfaces';
 
-export class Identity<T> implements ISetiod<T>, IFunctor<T> {
+export class Identity<T> implements ISetiod<T>, IApplicative<T> {
   constructor(private _value: T) {}
   value() {
     return this._value;
@@ -13,5 +13,14 @@ export class Identity<T> implements ISetiod<T>, IFunctor<T> {
   }
   map<U>(fn: (x: T) => U) {
     return new Identity<U>(fn(this.value()));
+  }
+  ap<U>(b: U) {
+    const f = this.value();
+    if (f instanceof Function) {
+      return Identity.of<U>((f as Function)(b));
+    }
+  }
+  static of<T>(value: T): Identity<T> {
+    return new Identity<T>(value);
   }
 }
--- ch11/src/interfaces/index.ts
+++ ch11/src/interfaces/index.ts
@@ -1,5 +1,9 @@
 import { IValuable } from './IValuable';
 import { ISetiod } from './ISetiod';
 import { IFunctor } from './IFunctor';
+import { IApplicative } from './IApplicative';
+import { IApply } from './IApply';
+import { IChain } from './IChain';
+import { IMonad } from './IMonad';
 
-export { IValuable, ISetiod, IFunctor };
+export { IValuable, ISetiod, IFunctor, IApplicative, IApply, IChain, IMonad };
