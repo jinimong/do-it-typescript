19a5c34ebd80ef4a3fa324530062610c187ad4d0
jwkim <phoongdeng@gmail.com>
2020-06-08 00:00:20 +0900

    IMonad<T> 인터페이스와 구현

--- ch11/src/classes/Identity.ts
+++ ch11/src/classes/Identity.ts
@@ -1,28 +1,38 @@
-import { IApplicative, IChain, ISetiod } from '../interfaces';
+import { ISetiod, IMonad } from '../interfaces';
 
-export class Identity<T> implements ISetiod<T>, IChain<T>, IApplicative<T> {
+export class Identity<T> implements ISetiod<T>, IMonad<T> {
   constructor(private _value: T) {}
   value() {
     return this._value;
   }
+
+  // ISetiod
   eqauls<U>(that: U): boolean {
     if (that instanceof Identity) {
       return this.value() == that.value();
     }
     return false;
   }
+
+  // IFunctor
   map<U>(fn: (x: T) => U) {
     return new Identity<U>(fn(this.value()));
   }
+
+  // IApply
   ap<U>(b: U) {
     const f = this.value();
     if (f instanceof Function) {
       return Identity.of<U>((f as Function)(b));
     }
   }
+
+  // IApplicative
   static of<T>(value: T): Identity<T> {
     return new Identity<T>(value);
   }
+
+  // IChain
   chain<U>(fn: (T) => U): U {
     return fn(this.value());
   }
